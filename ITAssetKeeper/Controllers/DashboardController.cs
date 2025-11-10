1d1bc738a8dad50a89f7e7d509435f8c52b5445c
Nao <ghost1.2.7011410n@gmail.com>
2025-11-10 16:14:19 +0900

    DashboardController の Adminアクションのデータ取得とビューへ渡すデータ処理の変更

--- ITAssetKeeper/Controllers/DashboardController.cs
+++ ITAssetKeeper/Controllers/DashboardController.cs
@@ -22,10 +22,13 @@ public class DashboardController : Controller
     [HttpGet]
     public async Task<IActionResult> Admin()
     {
-        // Deviceテーブルから新規登録された直近5件分を取得
+        // Deviceテーブルから全てのデータを取得する
+        var devices = await _context.Devices.ToListAsync();
+
+        // 取得したDeviceテーブルのデータから新規登録された直近5件分を取得
         // 登録日で降順にソート、先頭から5件分
         // DTO型でデータを詰める
-        var recentlyAdded5 = _context.Devices
+        var recentlyAdded5 = devices
             .OrderByDescending(x => x.CreatedAt)
             .Take(5)
             .Select(x => new DeviceRecentlyAddedDto
@@ -38,14 +41,15 @@ public class DashboardController : Controller
                 Location = x.Location,
                 UserName = x.UserName,
                 Status = x.Status
-            });
+            })
+            .ToList();
 
         // DeviceHistoryテーブルから直近7日分の件数を日別で取得
         // 当日から7日前までを取得し、更新日で降順にソート
         // 更新日でグルーピングし、件数を取得する
         var historyLast7days = _context.DeviceHistory
             .Where(x => x.UpdatedAt >= DateTime.Now.AddDays(-7))
-            .OrderByDescending(x => x.UpdatedAt.Date)
+            .OrderByDescending(x => x.UpdatedAt)
             .GroupBy(g => g.UpdatedAt.Date)
             .Select(g => new DeviceHistoryLast7DaysDto
             {
@@ -53,8 +57,8 @@ public class DashboardController : Controller
                 DisplayDate = g.Key.ToString("yyyy/MM/dd")
             });
 
-        // Deviceテーブルから機器の状態の件数を取得
-        var status = _context.Devices.Select(x => x.Status);
+        // 取得したDeviceテーブルのデータから機器の状態の件数を取得
+        var status = devices.Select(x => x.Status);
 
         // DashboardAdminViewModelのインスタンス生成
         // StatusCountを該当のDTO型で初期化しておく
@@ -65,17 +69,17 @@ public class DashboardController : Controller
 
         // 取得した情報をViewModelに詰めていく
         // 最近追加された機器一覧(5件)
-        adminVM.RecentlyAddedDevices = await recentlyAdded5.ToListAsync();
+        adminVM.RecentlyAddedDevices = recentlyAdded5;
 
         // 過去7日間の更新履歴件数
         adminVM.HistoryLast7Days = await historyLast7days.ToListAsync();
 
         // 状態別の台数集計
-        adminVM.StatusCount.WorkingCount = await status.CountAsync(x => x == DeviceStatus.Working.ToString());
-        adminVM.StatusCount.ReserveCount = await status.CountAsync(x => x == DeviceStatus.Reserve.ToString());
-        adminVM.StatusCount.BrokenCount = await status.CountAsync(x => x == DeviceStatus.Broken.ToString());
-        adminVM.StatusCount.ScheduledForDisposalCount = await status.CountAsync(x => x == DeviceStatus.ScheduledForDisposal.ToString());
-        adminVM.StatusCount.DisposedCount = await status.CountAsync(x => x == DeviceStatus.Disposed.ToString());
+        adminVM.StatusCount.WorkingCount = status.Count(x => x == DeviceStatus.Working.ToString());
+        adminVM.StatusCount.ReserveCount = status.Count(x => x == DeviceStatus.Reserve.ToString());
+        adminVM.StatusCount.BrokenCount = status.Count(x => x == DeviceStatus.Broken.ToString());
+        adminVM.StatusCount.ScheduledForDisposalCount = status.Count(x => x == DeviceStatus.ScheduledForDisposal.ToString());
+        adminVM.StatusCount.DisposedCount = status.Count(x => x == DeviceStatus.Disposed.ToString());
 
         // 実行結果をビューに返す
         return View(adminVM);
