d471672d9decaff193c84d902daf89df0c7bfdd4
Nao <ghost1.2.7011410n@gmail.com>
2025-11-11 15:28:01 +0900

    他RoleのSeedアカウントを追加、CreateAsync失敗時のログ出力を追加

--- ITAssetKeeper/Constants/ApplicationIdentityConstants.cs
+++ ITAssetKeeper/Constants/ApplicationIdentityConstants.cs
@@ -4,7 +4,16 @@
 public class ApplicationIdentityConstants
 {
     public static readonly int PASSWORD_VALID_DAYS = 42;
-    public static readonly string DEFAULT_NAME = "admin";
-    public static readonly string DEFAULT_EMAIL = "admin@example.com";
-    public static readonly string DEFAULT_PASSWORD = "Admin@123";
+
+    public static readonly string ADMIN_DEFAULT_NAME = "admin";
+    public static readonly string ADMIN_DEFAULT_EMAIL = "admin@example.com";
+    public static readonly string ADMIN_DEFAULT_PASSWORD = "Admin@123!";
+
+    public static readonly string EDITOR_DEFAULT_NAME = "editor";
+    public static readonly string EDITOR_DEFAULT_EMAIL = "editor@example.com";
+    public static readonly string EDITOR_DEFAULT_PASSWORD = "Editor@123!";
+
+    public static readonly string VIEWER_DEFAULT_NAME = "viewer";
+    public static readonly string VIEWER_DEFAULT_EMAIL = "viewer@example.com";
+    public static readonly string VIEWER_DEFAULT_PASSWORD = "Viewer@123!";
 }
--- ITAssetKeeper/Data/DbInitializer.cs
+++ ITAssetKeeper/Data/DbInitializer.cs
@@ -22,24 +22,75 @@ public class DbInitializer
         }
 
         // すでに admin があれば何もしないで抜ける
-        if (await userManager.FindByNameAsync(ApplicationIdentityConstants.DEFAULT_NAME) != null)
+        if (await userManager.FindByNameAsync(ApplicationIdentityConstants.ADMIN_DEFAULT_NAME) != null)
         {
             return;
         }
 
         // 作成する Seed Adminユーザーの設定
-        string adminUserName = ApplicationIdentityConstants.DEFAULT_NAME;
+        string adminUserName = ApplicationIdentityConstants.ADMIN_DEFAULT_NAME;
         var adminUser = new ApplicationUser
         {
             UserName = adminUserName,
-            Email = ApplicationIdentityConstants.DEFAULT_EMAIL,
+            Email = ApplicationIdentityConstants.ADMIN_DEFAULT_EMAIL,
+            EmailConfirmed = true,
+            IsActive = true,
+            PasswordExpirationDate = DateTime.Now.AddDays(ApplicationIdentityConstants.PASSWORD_VALID_DAYS)
+        };
+
+        // 作成する Seed Editorユーザーの設定
+        string editorUserName = ApplicationIdentityConstants.EDITOR_DEFAULT_NAME;
+        var editorUser = new ApplicationUser
+        {
+            UserName = editorUserName,
+            Email = ApplicationIdentityConstants.EDITOR_DEFAULT_EMAIL,
+            EmailConfirmed = true,
+            IsActive = true,
+            PasswordExpirationDate = DateTime.Now.AddDays(ApplicationIdentityConstants.PASSWORD_VALID_DAYS)
+        };
+
+        // 作成する Seed Viewerユーザーの設定
+        string viewerUserName = ApplicationIdentityConstants.VIEWER_DEFAULT_NAME;
+        var viewerUser = new ApplicationUser
+        {
+            UserName = viewerUserName,
+            Email = ApplicationIdentityConstants.VIEWER_DEFAULT_EMAIL,
             EmailConfirmed = true,
             IsActive = true,
             PasswordExpirationDate = DateTime.Now.AddDays(ApplicationIdentityConstants.PASSWORD_VALID_DAYS)
         };
 
         // Seed Adminの作成とRoleの追加を実施
-        await userManager.CreateAsync(adminUser, ApplicationIdentityConstants.DEFAULT_PASSWORD);
+        var adminResult = await userManager.CreateAsync(adminUser, ApplicationIdentityConstants.ADMIN_DEFAULT_PASSWORD);
+        if (!adminResult.Succeeded)
+        {
+            foreach (var err in adminResult.Errors)
+            {
+                Console.WriteLine($"ADMIN ERROR: {err.Description}");
+            }
+        }
         await userManager.AddToRoleAsync(adminUser,Roles.Admin.ToString());
+
+        // Seed Editorの作成とRoleの追加を実施
+        var editorResult = await userManager.CreateAsync(editorUser, ApplicationIdentityConstants.EDITOR_DEFAULT_PASSWORD);
+        if (!editorResult.Succeeded)
+        {
+            foreach (var err in editorResult.Errors)
+            {
+                Console.WriteLine($"EDITOR ERROR: {err.Description}");
+            }
+        }
+        await userManager.AddToRoleAsync(editorUser, Roles.Editor.ToString());
+
+        // Seed Viewerの作成とRoleの追加を実施
+        var viewerResult = await userManager.CreateAsync(viewerUser, ApplicationIdentityConstants.VIEWER_DEFAULT_PASSWORD);
+        if (!viewerResult.Succeeded)
+        {
+            foreach (var err in viewerResult.Errors)
+            {
+                Console.WriteLine($"VIEWER ERROR: {err.Description}");
+            }
+        }
+        await userManager.AddToRoleAsync(viewerUser, Roles.Viewer.ToString());
     }
 }
