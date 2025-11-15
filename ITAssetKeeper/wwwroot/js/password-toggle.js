e3f3a5464647acd55b606c6879d95c2fb70205e9
Nao <ghost1.2.7011410n@gmail.com>
2025-11-15 18:38:05 +0900

    ログイン画面、パスワード変更画面のデザイン修正

---
+++ ITAssetKeeper/wwwroot/js/password-toggle.js
@@ -0,0 +1,14 @@
+﻿$(document).on("click", ".password-toggle img", function () {
+    const $img = $(this);
+    const $input = $img.closest(".input-group").find("input");
+
+    if ($input.attr("type") === "password") {
+        // パスワード → 表示へ
+        $input.attr("type", "text");
+        $img.attr("src", "/img/eye_on.png");
+    } else {
+        // 表示 → パスワードへ
+        $input.attr("type", "password");
+        $img.attr("src", "/img/eye_off.png");
+    }
+});
